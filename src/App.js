aad80de00adac806cab89219d1a9cb5e5c600fae
Julio Cesar <thisjulio@gmail.com>
2019-09-22 05:33:43 -0300

    Signup Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Initial from './Screen/Initial';
+import SignUp from './Screen/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <Initial />
+      <SignUp />
     </div>
   );
 }
--- src/Screen/SignUp/index.js
+++ src/Screen/SignUp/index.js
@@ -5,14 +5,17 @@ import SideBySide from '../../Component/SideBySide';
 import CoverImage from '../../Component/CoverImage';
 import Button from '../../Component/Button';
 import Text from '../../Component/Text';
+import InputText from '../../Component/InputText';
 import ClickableOpacity from '../../Component/ClickableOpacity';
 
-import image1 from '../../assets/img/image1.png'
+import image2 from '../../assets/img/image2.png'
 
 import "./style.css"
 import Logo from "../../Component/Logo";
 
-const LeftContent = ({ ...props }) => (<CoverImage image={image1} />);
+const LeftContent = ({ ...props }) => (<CoverImage image={image2}>
+	<Logo color="#FFFFFF" />
+</CoverImage>);
 
 const RightContent = ({ ...props }) => (<div className="RightContent">
 	<div className="Login">
@@ -21,13 +24,18 @@ const RightContent = ({ ...props }) => (<div className="RightContent">
 		</ClickableOpacity>
 	</div>
 	<div className="SignUpContainer">
-		<div className="SignUp">
-			<Logo color="#F9314E" />
+		<div className="SignUpForm">
 			<div className="MainText">
 				<Text size={32}>
-					A New way<br /> to <b>management</b><br /> you <b>hackathon</b><br /> audience <b>together</b><br /> with <b>sponsors</b>
+					<strong>Sign Up</strong><br /> It’s quick and easy
 				</Text>
 			</div>
+			<InputText label="Name:" />
+			<InputText label="Email:" />
+			<InputText label="Work:" />
+			<InputText label="Role:" />
+			<InputText label="Category:" />
+			<InputText label="portfolio:" />
 			<Button title="Sign up" />
 		</div>
 	</div>
@@ -36,7 +44,7 @@ const RightContent = ({ ...props }) => (<div className="RightContent">
 	<InputText label="Senha" type="password" onChangeText={console.log} /> */}
 </div>);
 
-export default ({...props}) => (<div className="Initial">
+export default ({...props}) => (<div className="SignUp">
 	<Main>
 		<SideBySide leftContent={<LeftContent />} rightContent={<RightContent />} />
 	</Main>
